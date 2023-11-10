95d6ae2761ba4a4b0daf8b6e68bd8ad62e9cbc6e
Samuel Sciberras <49656532+samuelsciberras@users.noreply.github.com>
2023-11-10 16:22:38 +0100

    Added rolling dynamic text

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-
 function scrollToElement(elementId) {
-    var element = document.getElementById(elementId);
-    element.scrollIntoView();
-  }
+  var element = document.getElementById(elementId);
+  element.scrollIntoView();
+}
+
+const texts = [
+  "Software Engineer",
+  "Solutions Architect",
+  "Computer Vision Enthusiast",
+  "Machine Learning Engineer",
+];
+let count = 0;
+
+function changeText() {
+  const dynamicText = document.getElementById("dynamic-text");
+
+  setTimeout(() => {
+    dynamicText.textContent = texts[count];
+    count = (count + 1) % texts.length;
+  }, 1000);
+}
+
+setInterval(changeText, 3000);
