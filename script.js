f33ed70d969f50526dda667f50a169f6cd21b20a
Samuel Sciberras <49656532+samuelsciberras@users.noreply.github.com>
2023-11-10 18:52:13 +0100

    Add more information and new technical section

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ function changeText() {
 }
 
 setInterval(changeText, 3000);
+
+function openCategory(evt, categoryName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(categoryName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+window.onload = () => {
+  document.getElementById("languagesSkill").click();
+};
