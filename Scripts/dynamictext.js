45ba9ca28c5cb1efe5912f34fac6ceae273424d0
Samuel Sciberras <samuelsciberras96@gmail.com>
2023-11-11 23:00:27 +0100

    Fade in and out

--- Scripts/dynamictext.js
+++ Scripts/dynamictext.js
@@ -1,18 +1,27 @@
 const texts = [
-    "Software Engineer",
     "Computer Vision Enthusiast",
     "Solutions Architect",
     "Machine Learning Engineer",
+    "Software Engineer"
 ];
 let count = 0;
 
 function changeText() {
     const dynamicText = document.getElementById("dynamic-text");
+    // Start fade out
+    dynamicText.style.opacity = 0;
 
     setTimeout(() => {
+        // Change text and start fade in
         dynamicText.textContent = texts[count];
+        dynamicText.style.opacity = 1;
         count = (count + 1) % texts.length;
-    }, 1000);
+    }, 500); 
 }
 
-setInterval(changeText, 3000);
+setInterval(changeText, 4000);
+
+window.onload = () => {
+    const dynamicText = document.getElementById("dynamic-text");
+    dynamicText.style.opacity = 1;
+};
